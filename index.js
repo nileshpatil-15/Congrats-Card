a4a66244340b404e2e0a4366470eab21fde3d2e2
Nilesh <nileshpatil11115@gmail.com>
2023-04-16 03:27:20 +0000

    congrats

---
+++ index.js
@@ -0,0 +1,14 @@
+
+const element=(
+    <h1>congratulations</h1>
+    <div className='container'>
+          <img src='https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png'/>
+    <h1>kyu v</h1>
+    <h1>IIT Bombay</h1>
+    <img src='https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png'/>
+
+    </div> 
+)        
+  
+
+ReactDOM.render(element, document.getElementById("root"));
